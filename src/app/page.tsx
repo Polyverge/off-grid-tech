28ed13ceb87e7e2188020297ca9daa5ff7d569ee
Oliver Boehm <oliverboehmprivat@gmail.com>
2025-07-23 17:09:10 +0200

    Feat: Add link to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
 import { Button } from '@/components/ui/button';
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Home() {
   return (
-    <section id="main">
-      <h1>Ready to be free?</h1>
-      <Button>Start here</Button>
+    <section
+      id="main"
+      className="flex flex-col h-[100vh] items-center justify-center"
+    >
+      <h1 className="text-2xl mb-8 font-bold uppercase">Ready to be free?</h1>
+      <Link href={'app'}>
+        <Button className="cursor-pointer">Start here</Button>
+      </Link>
     </section>
   );
 }
