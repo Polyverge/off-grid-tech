1c83ca3a9d2bd0cd7723d614172f8b2fe05ad0a6
Oliver Boehm <oliverboehmprivat@gmail.com>
2025-07-23 17:01:37 +0200

    Refactor: format doc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,11 @@
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+import { Button } from '@/components/ui/button';
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <section id="main">
-    <h1>Ready to be free?</h1>
-    <Button>Start here</Button>
+      <h1>Ready to be free?</h1>
+      <Button>Start here</Button>
     </section>
   );
 }
