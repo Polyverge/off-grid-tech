8bc54b989451aa993baf3e5a2cb9c6e6546549db
Oliver Boehm <oliverboehmprivat@gmail.com>
2025-07-24 12:37:03 +0200

    Feat: Redirect to house

--- src/app/config/house/page.tsx
+++ src/app/config/house/page.tsx
@@ -1,5 +1,16 @@
-import { Section } from "lucide-react";
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { redirect } from 'next/navigation';
 
 export default function Page() {
-    return (<section></section>)
+  const redirectUrl = 'app/house/location';
+  redirect(redirectUrl);
+  return (
+    <section className="flex flex-col text-center">
+      <h1 className="mb-6">You'll be redirected shortly</h1>
+      <Link href={redirectUrl}>
+        <Button>Start here</Button>
+      </Link>
+    </section>
+  );
 }
