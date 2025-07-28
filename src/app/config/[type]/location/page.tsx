553b5f9ca1fa45134ac6b432c6c192210f1d66c3
Oliver Boehm <oliverboehmprivat@gmail.com>
2025-07-28 16:53:12 +0200

    Feat: Add temp image

--- src/app/config/[type]/location/page.tsx
+++ src/app/config/[type]/location/page.tsx
@@ -15,8 +15,9 @@ import { useState } from 'react';
 
 export default function Page() {
   const [location, setLocation] = useState({ latitude: 0, longitude: 0 });
+  const hasUserLocation = location.latitude !== 0;
   return (
-    <section id="location" className="flex justify-center gap-20 items-center">
+    <section id="location" className="flex justify-center gap-50 items-center">
       <section id="headeline" className="mb-15">
         <h2 className="text-2xl mb-5 font-bold">Where you at?</h2>
         <h3 className="text-lg">
@@ -32,19 +33,30 @@ export default function Page() {
           </CardTitle>
         </CardHeader>
         <CardContent>
-          <div className="w-90 h-60 bg-gray-200 rounded flex items-center justify-center">
-            {location.latitude === 0 ? (
+          <div
+            className="w-90 h-60 bg-gray-200 rounded flex items-center justify-center bg-cover bg-center"
+            style={
+              hasUserLocation
+                ? {
+                    backgroundImage:
+                      "url('https://picsum.photos/id/212/1800/1200')",
+                  }
+                : {}
+            }
+          >
+            {hasUserLocation ? (
+              <div className="backdrop-blur-xs bg-gray-500/30 p-7 rounded">
+                <p className="font-bold">You live in:</p>
+                <p className="text-primary">Berlepschstraße 6</p>
+                <p className="text-primary">34119 Kassel, Germany</p>
+              </div>
+            ) : (
               <MapPin
                 fill="#d45252"
                 stroke={'#000'}
                 strokeWidth={0.5}
                 size={80}
               />
-            ) : (
-              <div>
-                <p className="text-secondary"> {location.latitude}</p>
-                <p className="text-secondary"> {location.longitude}</p>
-              </div>
             )}
           </div>
         </CardContent>
